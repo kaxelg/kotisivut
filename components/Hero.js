2a2d2e6bf2cd08b1d6d9e7a7edeec089211c98de
kaxelg <kalle.gustafsson1@gmail.com>
2023-01-20 00:21:20 +0200

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,22 +4,22 @@ import { Cursor, useTypewriter } from 'react-simple-typewriter'
 function Hero() {
     const [text, count] = useTypewriter({
         words: [
-            "design and architecture.",
-            "sports.",
-            "people.",
-            "technology.",
-            "dogs.",
-            "problem solving."
+            "Kalle Gustafsson.",
+            "future UX designer.",
+            "design enthousiast.",
+            "technology geek.",
+            "problem solver.",
+            "dog lover."
         ],
         loop: true,
         delaySpeed: 2000,
     });
   return (
     <div className="h-96 grid grid-cols-1 gap-4 content-center pt pl-10 text-3xl snap-center">
-        <h1>Hi, I am Kalle Gustafsson.</h1>
-        <h1>I enjoy <span className='text-red-800'>{text}</span>
+        <h1>Hi, I am <span className='text-red-800'>{text}</span>
         <Cursor cursorColor="#000000" />
-        </h1> 
+        </h1>
+        <h1>Welcome to my portfolio!</h1> 
     </div>
   )
 }
