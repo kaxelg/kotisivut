c95cd9bfd1bdec0c43870bc64088eabc49ad7fe3
kaxelg <kalle.gustafsson1@gmail.com>
2023-01-20 00:11:07 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,16 @@
 import React from 'react'
-import { SocialIcon } from 'react-social-icons';
 import Link from 'next/link';
 
 function Header() {
   return (
     <header className="top-0 p-5 flex items-start justify-between z-20">
         <div className="flex flex-row items-center pt-2">
-            <Link className='pr-10' href="#projects">Projects</Link>
-            <Link href="#skills">Skills</Link>
+            <Link className='uppercase text-sm pr-10' href="#projects">Projects</Link>
+            <Link className="uppercase text-sm pr-10" href="#skills">Skills</Link>
+            <Link className="uppercase text-sm" href='mailto:kalle.gustafsson@tuni.fi'>download cv</Link>
         </div>
         <div className="flex flex-row items-center cursor-pointer">
-            {/* Social Icon */}
-            <a>
-              <SocialIcon 
-                className="cursor-pointer"
-                network='email'
-                fgColor='gray'
-                bgColor='transparent'
-              />
-            </a>
-            
-            <a href='mailto:kalle.gustafsson@tuni.fi' className="uppercase hidden md:inline-flex text-sm ">Contact me</a>
+            <Link href='mailto:kalle.gustafsson@tuni.fi' className="uppercase text-sm pr-10">Contact me</Link>
         </div>
     </header>
   )
