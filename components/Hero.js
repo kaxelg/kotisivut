32a7fa5e91da9ede9c4b94cf3c3b04106a1fa530
kaxelg <kalle.gustafsson1@gmail.com>
2023-01-20 00:15:09 +0200

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -16,7 +16,7 @@ function Hero() {
     });
   return (
     <div className="h-96 grid grid-cols-1 gap-4 content-center pt pl-10 text-3xl snap-center">
-        <h1>Hi, I'm Kalle Gustafsson.</h1>
+        <h1>Hi, I am Kalle Gustafsson.</h1>
         <h1>I enjoy <span className='text-red-800'>{text}</span>
         <Cursor cursorColor="#000000" />
         </h1> 
